8e6878cedadd20558a77bb622adb506c462c86cb
pushpendra8082 <143595507+pushpendra8082@users.noreply.github.com>
2023-08-30 14:57:11 +0530

    check pallindrome

--- pallindrome.js
+++ pallindrome.js
@@ -3,7 +3,7 @@ function palindrome(str) {
     var len = str.length;
     var mid = Math.floor(len/2);
 
-    for ( var i = 1; i < mid; i++ ) {
+    for ( var i = 0; i < mid; i++ ) {
         if (str[i] !== str[len - 1 - i]) {
             return false;
         }
